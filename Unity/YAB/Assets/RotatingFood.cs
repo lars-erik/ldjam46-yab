0c952816e995a36653f411d7f8af2f2c25fdbe55
Lars-Erik Aabech <lea@markedspartner.no>
2020-04-18 19:17:47 +0200

    Initial commit

---
+++ Unity/YAB/Assets/RotatingFood.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingFood : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rotation = Time.deltaTime * 180f;
+        gameObject.transform.Rotate(rotation, 0, 0);
+    }
+}
